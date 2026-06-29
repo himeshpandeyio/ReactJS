d9d92e7382174f823c21395180b898bfabbf8007
Himesh <pandeyhimesh65@gmail.com>
2026-06-30 01:12:26 +0545

    Updated program running successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import MovieCard from "./components/MovieCard";
+import MovieBanner from "./components/MovieBanner";
+import SearchBar from "./components/SearchBar";
 import movie from "./components/data";
 
-function MovieBanner() {
-  return (
-    <div className="banner">
-      {movie.map((m) => (
-        <div key={m.id}>
-          <h2>{m.title}</h2>
-          <p>Rating: {m.rating}</p>
+function App (){
+
+    return (
+        <div className="container">
+            <MovieCard />
+            <MovieBanner />
+            <SearchBar />
         </div>
-      ))}
-    </div>
-  );
+    )
 }
 
-export default MovieBanner;
+export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,18 @@
 import { useState } from "react";
 
-function SearchBar(){
-    const [query, setQuery] = useState("");
+function SearchBar() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="...Search movies..."
+      />
+      <p>You typed: {query}</p>
+    </div>
+  );
 }
+
+export default SeacrchBar;
