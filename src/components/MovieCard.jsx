54f642e9ade559c5a625ff7be01fc4d9f94f2448
Himesh <pandeyhimesh65@gmail.com>
2026-06-30 22:35:58 +0545

    Updated program running successfully

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,34 +1,6 @@
+import movie from "./data";
+
 function MovieCard() {
-  const movie = [
-    {
-      id: 1,
-      title: "Inception",
-      rating: 8.8,
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-    },
-    {
-      id: 2,
-      title: "Interstellar",
-      rating: 8.6,
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
-    },
-    {
-      id: 3,
-      title: "The Wolverine",
-      rating: 8.5,
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/The_Wolverine_posterUS.jpg/250px-The_Wolverine_posterUS.jpg",
-    },
-    {
-      id: 4,
-      title: "Troy",
-      rating: 8.4,
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Troy_03500296.png/250px-Troy_03500296.png",
-    },
-  ];
 
   return (
     <div className="container">
---
+++ src/components/data.jsx
@@ -0,0 +1,60 @@
+  const movie = [
+    {
+      id: 1,
+      title: "Inception",
+      rating: 8.8,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
+    },
+    {
+      id: 2,
+      title: "Interstellar",
+      rating: 8.6,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+    },
+    {
+      id: 3,
+      title: "The Wolverine",
+      rating: 8.5,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/The_Wolverine_posterUS.jpg/250px-The_Wolverine_posterUS.jpg",
+    },
+    {
+      id: 4,
+      title: "Troy",
+      rating: 8.4,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Troy_03500296.png/250px-Troy_03500296.png",
+    },
+        {
+      id: 5,
+      title: "Dhurandhar",
+      rating: 8.4,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/Dhurandhar_poster.jpg/250px-Dhurandhar_poster.jpg",
+    },
+        {
+      id: 6,
+      title: "Dhurandhar-The Revenge",
+      rating: 8.5,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Dhurandhar-_The_Revenge_poster.jpg/250px-Dhurandhar-_The_Revenge_poster.jpg",
+    },
+        {
+      id: 7,
+      title: "Gladiator",
+      rating: 8.5,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/Gladiator_%282000_film_poster%29.png/250px-Gladiator_%282000_film_poster%29.png",
+    },
+        {
+      id: 8,
+      title: "The Terminator",
+      rating: 8.4,
+      poster:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/The_Terminator.png/250px-The_Terminator.png",
+    },
+  ];
+
+  export default movie;
