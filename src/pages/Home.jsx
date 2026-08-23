c620cbc74f54092432879dcb814582a616231502
Himesh <pandeyhimesh65@gmail.com>
2026-08-24 01:13:55 +0545

    Updating the code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,38 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 const Home = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
-    { id: 1, title: "Movie 1", release_date: "2010-07-16" },
-    { id: 2, title: "Movie 2", release_date: "2010-07-16" },
-    { id: 3, title: "Movie 3", release_date: "2010-07-16" },
-    { id: 4, title: "Movie 4", release_date: "2010-07-16" },
-    { id: 5, title: "Movie 5", release_date: "2010-07-16" },
+    { id: 1, title: "Inception", release_date: "2010-07-16" },
+    { id: 2, title: "Interstellar", release_date: "2010-07-18" },
+    { id: 3, title: "Gladiator", release_date: "2010-07-19" },
+    { id: 4, title: "Thor: Ragnarok", release_date: "2010-07-26" },
+    { id: 5, title: "Pursuit of Happyness", release_date: "2010-07-15" },
   ];
 
   const handleSearch = (event) => {
     event.preventDefault();
-  }
+    alert(`Searching for: ${searchQuery}`);
+  };
   return (
     <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for a movie..." className="search-input"/>
+            <input 
+            type="text" 
+            placeholder="Search for a movie..." 
+            className="search-input"
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}/>
             <button type="submit" className="search-button">Search</button>
         </form>
       <div className="movies-grid">
         {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} key={movie.id} />
+            movie.title.toLowerCase().startsWith(searchQuery)
+            &&
+          <MovieCard movie={movie} key={movie.id} />
         ))}
       </div>
     </div>
