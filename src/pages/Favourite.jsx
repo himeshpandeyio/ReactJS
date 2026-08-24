edb7a97915e86d0759e98aaa84731755f3501b01
Himesh <pandeyhimesh65@gmail.com>
2026-08-24 22:50:30 +0545

    updated components

---
+++ src/pages/Favourite.jsx
@@ -0,0 +1,11 @@
+
+const Favourite = () => {
+  return (
+    <div className="favourites-empty">
+        <h2>No Favourite Movies</h2>
+        <p>Click the heart icon on a movie to add it to your favourites.</p>
+    </div>
+  )
+}
+
+export default Favourite
