4527ef63b27962aece5e66b32244400911052800
Himesh <pandeyhimesh65@gmail.com>
2026-09-03 21:13:24 +0545

    MovieFlix app code updated

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -1,12 +1,29 @@
 import "../css/Favourite.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 const Favourite = () => {
-  return (
-    <div className="favourites-empty">
+  const { favorites } = useMovieContext();
+
+  if (favorites.length === 0) {
+    return (
+      <div className="favourites-empty">
         <h2>No Favourite Movies</h2>
         <p>Click the heart icon on a movie to add it to your favourites.</p>
-    </div>
-  )
-}
+      </div>
+    );
+  }
 
-export default Favourite
+  return (
+    <div className="favourites">
+      <h2>Your Favourites</h2>
+      <div className="movies-grid">
+        {favorites.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Favourite;
